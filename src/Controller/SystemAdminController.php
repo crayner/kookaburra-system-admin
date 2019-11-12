a53e37b25bcdb7f90d70ff63ac0ac166358d142f
Craig Rayner <craig@craigrayner.com>
2019-11-13 07:13:12 +1100

    check does not need request

--- src/Controller/SystemAdminController.php
+++ src/Controller/SystemAdminController.php
@@ -307,7 +307,7 @@ class SystemAdminController extends AbstractController
      * @Route("/check/", name="check")
      * @IsGranted("ROLE_ROUTE")
      */
-    public function check(VersionManager $manager, Request $request)
+    public function check(VersionManager $manager)
     {
         return $this->render('@KookaburraSystemAdmin/check.html.twig',
             [
